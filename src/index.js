b1e32e6df776eaa2e76bdd7a947ea71c00b07caf
Stijn Hellem <stijn.hellem@student.howest.bnegit config --global user.email stijn.hellem@student.howest.be>
2019-10-18 14:13:38 +0200

    JSON fetch & created <li> items

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import './style.css';
 import {devineLog} from './js/utility/helpers';
+import data from './assets/data/coffees.json';
 
+console.log(data);
+console.log(data.coffees);
+
+data.coffees.forEach(drink => {
+  if (drink.plantbased === true) {
+    document.querySelector(`.prices__list`).innerHTML += `
+    <li class="price data-id="${drink.id}">
+    <a class="price__button">
+      <span class="price__button__wrapper">
+        <span class="price__button__name">${drink.name}</span>
+        <span class="price__button__amount">&euro; ${drink.prices.medium}</span>
+      </span>
+      <span class="price__button__plus">+</span>
+    </a>
+    </li>
+    `;
+  }
+});
 devineLog('Hey, ik ben een JS file');
 const arr = [1, 2,  3];
 
