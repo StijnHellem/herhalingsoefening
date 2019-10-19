e8c6e9e54233dc0dbc9e19b1ccdeb8413920c0f8
Stijn Hellem <stijn.hellem@student.howest.bnegit config --global user.email stijn.hellem@student.howest.be>
2019-10-19 13:21:17 +0200

    js order amounts, total not ready yet

--- src/index.js
+++ src/index.js
@@ -4,31 +4,75 @@ import data from './assets/data/coffees.json';
 
 console.log(data);
 console.log(data.coffees);
-
-data.coffees.forEach(drink => {
-  if (drink.plantbased === true) {
-    document.querySelector(`.prices__list`).innerHTML += `
-    <li class="price data-id="${drink.id}">
+const plantbasedCoffees = data.coffees.filter(coffee => coffee.plantbased === true);
+console.log(plantbasedCoffees);
+plantbasedCoffees.forEach(drink => {
+  //console.log(parseInt(drink.id));
+  document.querySelector(`.prices__list`).innerHTML += `
+    <li class="price">
     <a class="price__button">
       <span class="price__button__wrapper">
         <span class="price__button__name">${drink.name}</span>
         <span class="price__button__amount">&euro; ${drink.prices.medium}</span>
       </span>
-      <span class="price__button__plus">+</span>
+      <span class="price__button__plus" data-id="${parseInt(drink.id) - 1}" data-amount="0">+</span>
     </a>
     </li>
     `;
-  }
+
 });
 
-console.log(document.querySelector(`.price__button__plus`));
-document.querySelector(`html`).addEventListener(`click`, handleClickField);
 
-document.querySelectorAll(`.price__button__plus`).forEach($button => $button.addEventListener(`click`, handleClickField));
+const handleClickField = e => {
+  e.target.setAttribute(`data-amount`, parseInt(e.target.dataset.amount) + 1);
 
-const handleClickField = () => {
-  console.log('click');
+  // console.log(data.coffees[e.target.dataset.id].prices.medium);
+  // console.log(plantbasedCoffees[e.target.dataset.id].name);
+
+  if (e.target.dataset.amount < 2) {
+    document.querySelector(`.orders`).innerHTML += `
+      <li class="order" data-id="${e.target.dataset.id}">
+                    <span class="order__name">
+                      <span class="order__amount">${e.target.dataset.amount}x</span> ${plantbasedCoffees[e.target.dataset.id].name}
+                    </span>
+                    <span class="order__price">&euro; ${plantbasedCoffees[e.target.dataset.id].prices.medium}</span>
+                    <button class="remove">
+                      x
+                    </button>
+                  </li>
+      `;
+  }
+  else {
+    document.querySelectorAll(`.order`).forEach(order => {
+
+      if (order.dataset.id === e.target.dataset.id) {
+
+        order.innerHTML = `
+        <span class="order__name">
+        <span class="order__amount">${e.target.dataset.amount}x</span> ${plantbasedCoffees[e.target.dataset.id].name}
+      </span>
+      <span class="order__price">&euro; ${e.target.dataset.amount * plantbasedCoffees[e.target.dataset.id].prices.medium}</span>
+      <button class="remove">
+        x
+      </button>`;
+      }
+    });
+    // document.querySelector(`.order__amount`).textContent = `${e.target.dataset.amount}`;
+  }
+  const $total = document.querySelectorAll(`.order__price`);
+  $total.forEach(order => {
+    console.log(order.textContent.split(' '));
+    const split = order.textContent.split(' ');
+    console.log(parseInt(split[1]));
+
+  });
 };
+
+
+
+document.querySelectorAll(`.price__button__plus`).forEach(button => {
+  button.addEventListener(`click`, handleClickField);
+});
 
 
 
